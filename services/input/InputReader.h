c983b246a1350b78dbd698b2afbbf4f495525587
Edward Peek <edwardpeek@gmail.com>
2012-04-04 06:17:00 +1200

    Added option to disable touchscreen pressure sensitivity from idc files

--- services/input/InputReader.h
+++ services/input/InputReader.h
@@ -1243,6 +1243,7 @@ protected:
             PRESSURE_CALIBRATION_NONE,
             PRESSURE_CALIBRATION_PHYSICAL,
             PRESSURE_CALIBRATION_AMPLITUDE,
+            PRESSURE_CALIBRATION_DISABLE,
         };
 
         PressureCalibration pressureCalibration;
